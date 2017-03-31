fe66132f9f28696393c5a16bafacf6f7c6c07773
Roland Urbano <urbano.roland@gmail.com>
2017-03-31 14:09:11 +0200

    update collector

--- collector.py
+++ collector.py
@@ -8,6 +8,7 @@ import datetime
 import json
 import hpfeeds
 import logging
+import re
 
 root = logging.getLogger()
 root.setLevel(logging.ERROR)
@@ -20,13 +21,20 @@ root.addHandler(ch)
 logger = logging.getLogger("hpfeeds-collector")
 
 def parse(line):
-    # TODO: extend this
-    return {}
+    regex = r''
+    match = re.match(regex, line)
+    if match:
+        res = match.groupdict()
+        for name in res.keys():
+            if not res[name]:
+                del res[name]
+        return res
+    return None
 
 def hpfeeds_connect(host, port, ident, secret):
     try:
         connection = hpfeeds.new(host, port, ident, secret)
-    except hpfeeds.FeedException, e:
+    except hpfeeds.FeedException as e:
         logger.error('feed exception: %s'%e)
         sys.exit(1)
     logger.info('connected to %s (%s:%s)'%(connection.brokername, host, port))
@@ -34,12 +42,12 @@ def hpfeeds_connect(host, port, ident, secret):
 
 def main():
     cfg = {
-        'host' : '',
+        'host' : '192.168.56.100',
         'port' : 10000,
-        'channel' : '',
-        'ident ' : '',
-        'secret' : '',
-        'tail_file' : ''
+        'channel' : 'samba',
+        'id' : 'captainhook',
+        'secret' : 'alibaba',
+        'tail_file' : '/var/log/samba/audit.log'
     }
 
     if len(sys.argv) > 1:
@@ -52,7 +60,7 @@ def main():
                 cfg[name] = value.encode("utf-8")
     else:
         logger.warning("Warning: no config found, using default values for hpfeeds server")
-    publisher  = hpfeeds_connect(cfg['host'], cfg['port'], cfg['ident'], cfg['secret'])
+    publisher  = hpfeeds_connect(cfg['host'], cfg['port'], cfg['id'], cfg['secret'])
 
     tail = multitail2.MultiTail(cfg['tail_file'])
     for filemeta, line in tail:
